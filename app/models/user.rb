cbc6d8f40504600514cb0e8623f6c9ba9a0a1ecc
NtwaliHeritier <heritierntwali@gmail.com>
2020-11-09 15:08:17 +0200

    add friendship model and follows

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :follower_users, foreign_key: :follower_id, class_name: "Friendship"
+  has_many :followees, through: :follower_users
+
+  has_many :followee_users, foreign_key: :followee_id, class_name: "Friendship"
+  has_many :followers, through: :followee_users
+
 end
