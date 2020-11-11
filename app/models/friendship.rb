fb2aed355127ab22f6ffeca63e8ac015bd83b3ef
NtwaliHeritier <heritierntwali@gmail.com>
2020-11-11 16:09:27 +0200

    update following as required by TSE

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -13,16 +13,10 @@ class Friendship < ApplicationRecord
   def self.mutual_friends(user1, user2)
     friends1 = Friendship.where("follower_id=?", user1.id).pluck(:followee_id)
     friends2 = Friendship.where("follower_id=?", user2.id).pluck(:followee_id)
-    # common_friends = friends1.select { |friend| friends2.include? friend }
-    # arr = []
-    # i = 0
-    # while i < common_friends.length
-    #   arr.push(User.find(common_friends[i]))
-    #   i += 1
-    # end
-    # arr
-    User.where( "id in (?)",
-    friends1.select { |friend| friends2.include? friend }
-    )
+    User.where( "id in (?)", friends1.select { |friend| friends2.include? friend })
+  end
+
+  def self.friendship_exists?(follower_id, followee_id)
+    Friendship.where("(follower_id=? and followee_id=?) or (followee_id=? and follower_id=?)", follower_id, followee_id, followee_id, follower_id).first
   end
 end
