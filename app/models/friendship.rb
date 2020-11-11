93f3918135ccb070821bfefcae8559eb88b6f4be
lamiasristy <lamiasristy@gmail.com>
2020-11-11 11:27:23 +0100

    Mutual friendship locis added.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -9,4 +9,18 @@ class Friendship < ApplicationRecord
   def self.request_confirmed(follower_id)
     Friendship.where("status=? and follower_id=?", true, follower_id)
   end
+
+  def self.mutual_friends(user1, user2)
+    friends1 = Friendship.where("follower_id=?", user1.id).pluck(:followee_id)
+    friends2 = Friendship.where("follower_id=?", user2.id).pluck(:followee_id)
+    common_friends = friends1.select{|friend| friends2.include?friend }
+    arr=[]
+    i=0
+    while i<common_friends.length do
+      arr.push(User.find(common_friends[i]))
+      i+=1
+    end
+    arr
+  end
+
 end
